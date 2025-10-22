9f78c44cf20ec58707ea2cde5088c9eae47f5ca6
Norly Pierre <norlypierre@gmail.com>
2025-10-22 10:20:18 -0400

    Added missing code and started room

---
+++ src/test/java/com/pluralsight/RoomTest.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import static org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+  private Room room;
+
+  void setRoom {
+      room = new Room(2,100)
+  }
+
+}
